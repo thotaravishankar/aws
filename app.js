114eb70d68c2395dd48efa2dbb44a83234033841
thotaravishankar <113172082+thotaravishankar@users.noreply.github.com>
2024-03-14 16:26:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(Server running on the port number ${PORT}));
+app.listen(PORT, console.log('Server running on the port number ${PORT}'));
 
 //TESTING
 app.get('/test', async function(req, res){
